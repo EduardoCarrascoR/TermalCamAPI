0d6cdd4f48ea41da1555c5e825b43498253993bf
Eduardo Carrasco <blezthor666@gmail.com>
2020-01-17 14:12:12 -0300

    Add webSocketsServer

--- TermalCamAPI.ino
+++ TermalCamAPI.ino
@@ -3,9 +3,14 @@
 #include <DNSServer.h>        //DNSServer.h .- Local WebServer usado para servir el portal de configuración (https://github.com/zhouhan0126/DNSServer---esp32)
 #include <WiFiManager.h>      //WiFiManager.h .- WiFi Configuration Magic (https://github.com/zhouhan0126/DNSServer---esp32) >> https://github.com/zhouhan0126/DNSServer---esp32 (ORIGINAL)
 #include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266mDNS.h>      // DNS
 #include <Adafruit_AMG88xx.h>
 #include <ArduinoJson.h>
 #include <Wire.h>
+#include <WebSocketsServer.h>
+
+//define serial port diferente
+#define USE_SERIAL Serial
 
 
 //low range of the sensor (this will be blue on the screen)
@@ -30,14 +35,33 @@ Adafruit_AMG88xx amg;
 
 
 ESP8266WebServer webServer(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 ESP8266HTTPUpdateServer httpUpdateServer;
 
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+
+  switch(type) {
+    case WStype_DISCONNECTED:
+      USE_SERIAL.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED: {
+      IPAddress ip = webSocket.remoteIP(num);
+      USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+        // send message to client
+      webSocket.sendTXT(num, "Connected");
+      }
+      break;
+        
+  }
+
+}
 void configModeCallback(WiFiManager *myWiFiManager)
 {
-  Serial.println("Modo de configuración ingresado");
-  Serial.println(WiFi.softAPIP());
+  USE_SERIAL.println("Modo de configuración ingresado");
+  USE_SERIAL.println(WiFi.softAPIP());
 
-  Serial.println(myWiFiManager->getConfigPortalSSID());
+  USE_SERIAL.println(myWiFiManager->getConfigPortalSSID());
 }
 
 //flag for saving data
@@ -48,13 +72,13 @@ bool shouldSaveConfig = false;
 
 void saveConfigCallback()
 {
-  Serial.println("Debería guardar la configuración");
+  USE_SERIAL.println("Debería guardar la configuración");
   shouldSaveConfig = true;
 }
 
 void setup()
 {
-  Serial.begin(115200);
+  USE_SERIAL.begin(115200);
   pinMode(PIN_AP, INPUT);
 
   //modificamos el portal
@@ -75,7 +99,9 @@ void setup()
   
   httpUpdateServer.setup(&webServer);
 
-  webServer.on("/", readCam);
+  // handle index
+
+  //webServer.on("/", readCam);
 
   webServer.on("/all", HTTP_GET, [](){
     webServer.sendHeader("Access-Control-Allow-Origin", "*");
@@ -84,10 +110,29 @@ void setup()
     webServer.send(200, "text/plain", "chico te paseo");
   });
 
-  webServer.begin(); // iniciamos el servidor web
+  webServer.on("/", []() {
+        // send index.html
+        webServer.send(200, "text/html", "<html><head><script>var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);connection.onopen = function () {  connection.send('Connect ' + new Date()); }; connection.onerror = function (error) {    console.log('WebSocket Error ', error);};connection.onmessage = function (e) {  console.log('Server: ', e.data);};function sendRGB() {  var r = parseInt(document.getElementById('r').value).toString(16);  var g = parseInt(document.getElementById('g').value).toString(16);  var b = parseInt(document.getElementById('b').value).toString(16);  if(r.length < 2) { r = '0' + r; }   if(g.length < 2) { g = '0' + g; }   if(b.length < 2) { b = '0' + b; }   var rgb = '#'+r+g+b;    console.log('RGB: ' + rgb); connection.send(rgb); }</script></head><body>LED Control:<br/><br/>R: <input id=\"r\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>G: <input id=\"g\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>B: <input id=\"b\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/></body></html>");
+    });
+
+
+  // start webSocket server
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+  // start MDNS server
+  if(MDNS.begin("esp8266")) {
+    USE_SERIAL.println("MDNS responder started");
+  }
 
   
+  // Add service to MDNS
+  MDNS.addService("http", "tcp", 80);
+  MDNS.addService("ws", "tcp", 81);
 }
+
+unsigned long last_10sec = 0;
+unsigned int counter = 0;
 
 void loop()
 {
@@ -104,25 +149,33 @@ void loop()
     }
     Serial.println("conectado ESP_AP!!!");
   } */
-
-  webServer.handleClient();
+  unsigned long t = millis();
   static bool hasConnected = false;
-    if (WiFi.status() != WL_CONNECTED) {
-      //      Serial.printf("Connecting to %s\n", ssid);
-      hasConnected = false;
-    }
-    else if (!hasConnected) {
-      hasConnected = true;
-      Serial.print("Connected! Open http://");
-      Serial.print(WiFi.localIP());
-      Serial.println(": in your browser");
-    }
+  if (WiFi.status() != WL_CONNECTED) {
+  //      Serial.printf("Connecting to %s\n", ssid);
+    hasConnected = false;
+  }
+  else if (!hasConnected) {
+    hasConnected = true;
+    USE_SERIAL.print("Connected! Open http://");
+    USE_SERIAL.print(WiFi.localIP());
+    USE_SERIAL.println(": in your browser");
+  }
+
+  webSocket.loop();
+  if((t - last_10sec) > 10 * 1000) {
+    counter++;
+    bool ping = (counter % 2);
+    int i = webSocket.connectedClients(ping);
+    USE_SERIAL.printf("%d Connected websocket clients ping: %d\n", i, ping);
+    last_10sec = millis();
+  }
 }
 
-void readCam() {
+void readCam(const char * payload, size_t length) {
   float temp;
   amg.readPixels(pixels);
-  Serial.println("chucoco");
+  USE_SERIAL.println("chucoco");
   StaticJsonDocument<200> doc;
   JsonArray data = doc.createNestedArray("data");
   for(int i=0; i<AMG88xx_PIXEL_ARRAY_SIZE; i++){
@@ -139,6 +192,6 @@ void readCam() {
       if(i==AMG88xx_PIXEL_ARRAY_SIZE-1)
       JSON.concat(String("}"));
   } */
-  Serial.println(Json);
+  USE_SERIAL.println(Json);
   webServer.send(200, "text/json", Json);
 }
