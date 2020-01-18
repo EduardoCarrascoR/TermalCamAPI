f55502d4571c058b852ec513f65294abaf5bb88e
Eduardo Carrasco <blezthor666@gmail.com>
2020-01-17 21:26:23 -0300

    correction
    use readCam and add webServer handle

--- TermalCamAPI.ino
+++ TermalCamAPI.ino
@@ -99,8 +99,8 @@ void setup()
   
   httpUpdateServer.setup(&webServer);
 
-  // handle index
-
+  // llamada a funcion readCam
+  readCam;
   //webServer.on("/", readCam);
 
   webServer.on("/all", HTTP_GET, [](){
@@ -111,11 +111,13 @@ void setup()
   });
 
   webServer.on("/", []() {
-        // send index.html
-        webServer.send(200, "text/html", "<html><head><script>var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);connection.onopen = function () {  connection.send('Connect ' + new Date()); }; connection.onerror = function (error) {    console.log('WebSocket Error ', error);};connection.onmessage = function (e) {  console.log('Server: ', e.data);};function sendRGB() {  var r = parseInt(document.getElementById('r').value).toString(16);  var g = parseInt(document.getElementById('g').value).toString(16);  var b = parseInt(document.getElementById('b').value).toString(16);  if(r.length < 2) { r = '0' + r; }   if(g.length < 2) { g = '0' + g; }   if(b.length < 2) { b = '0' + b; }   var rgb = '#'+r+g+b;    console.log('RGB: ' + rgb); connection.send(rgb); }</script></head><body>LED Control:<br/><br/>R: <input id=\"r\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>G: <input id=\"g\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>B: <input id=\"b\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/></body></html>");
-    });
+      // send index.html
+      webServer.send(200, "text/html", "<html><head><script>var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);connection.onopen = function () {  connection.send('Connect ' + new Date()); }; connection.onerror = function (error) {    console.log('WebSocket Error ', error);};connection.onmessage = function (e) {  console.log('Server: ', e.data);};function sendRGB() {  var r = parseInt(document.getElementById('r').value).toString(16);  var g = parseInt(document.getElementById('g').value).toString(16);  var b = parseInt(document.getElementById('b').value).toString(16);  if(r.length < 2) { r = '0' + r; }   if(g.length < 2) { g = '0' + g; }   if(b.length < 2) { b = '0' + b; }   var rgb = '#'+r+g+b;    console.log('RGB: ' + rgb); connection.send(rgb); }</script></head><body>LED Control:<br/><br/>R: <input id=\"r\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>G: <input id=\"g\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>B: <input id=\"b\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/></body></html>");
+  });
 
 
+  //start webServer
+  webServer.begin();
   // start webSocket server
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
@@ -150,6 +152,7 @@ void loop()
     Serial.println("conectado ESP_AP!!!");
   } */
   unsigned long t = millis();
+  webServer.handleClient();
   static bool hasConnected = false;
   if (WiFi.status() != WL_CONNECTED) {
   //      Serial.printf("Connecting to %s\n", ssid);
@@ -193,5 +196,5 @@ void readCam(const char * payload, size_t length) {
       JSON.concat(String("}"));
   } */
   USE_SERIAL.println(Json);
-  webServer.send(200, "text/json", Json);
+  webSocket.sendTXT(Json.c_str(), Json.length() + 1);
 }
